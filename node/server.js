a3df80f847f23935d77c1e97e6e8afdd79b0e945
Darius Washington <dariuswashington@amdg.rockhursths.edu>
2017-01-30 11:34:20 -0600

    Add gitignore

--- node/server.js
+++ node/server.js
@@ -1,24 +1,34 @@
 var http = require('http');
 var Mongo = require('mongodb').MongoClient;
 var express = require('express');
-var mongoUrl = 'mongodb://localhost:27017/myapp';
-Mongo.connect(mongoUrl, function (err, db) {
-    if (err) {
-        log('MongoDB connection error');
-    }
-    else {
-        log('Connected to MongoDB');
-    }
-    Mongo.ops = {};
+
+var mongo_url = 'mongodb://localhost:27017/apcsp';
+
+Mongo.connect(mongo_url, function(err, db) {
+ if(err) {
+   log('MongoDB connection error');
+ } else {
+   log('Connected to MongoDB');
+ }
+}); 
+
+// web server
+var app = express();
+
+// use middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(allowCrossDomain);
+app.use(authorize);
+
+app.post('/login', function (req, res) {
+ log('/login req.body = ', req.body);
+ var query = { id : req.body.id };
+ Mongo.ops.upsert('login', query, req.body);
+ res.status(201).send('ok');
 });
 
-function log(msg) {
-    console.log(msg);
-}
-var app = express();
-app.get('/', function (req, res) {
-    res.send('hello');
-});
-app.listen(3000, function () {
-    log('listening on port 3000');
-});
+// listen on port 3000
+app.listen(3000);
+
+log('listening on port 3000');
